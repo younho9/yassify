558bb22d1e51c8d52e84b5bd075e74e9e6e5602b
Younho Choo <yo+github@younho9.com>
2025-02-08 15:01:46 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# yassify
+
+Use `yassify` to apply advanced beauty filters to any string of text.
+
+## License
+
+MIT
---
+++ mod.ts
@@ -0,0 +1,9 @@
+/**
+ * Yassify a string of text by appending emoji
+ *
+ * @param str The string of text to yassify.
+ * @returns a string of text with emoji appended
+ */
+export function yassify(str: string): string {
+  return `${str} 💅✨👑`;
+}
